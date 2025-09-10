c3ba0be89f3d4956183f855493b19b1a753cfd9d
Bansal-Karan <bansalkaran94651@gmail.com>
2025-09-10 19:40:36 +0530

    Fix image source paths in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,19 +3,19 @@ const projects = [
     {
         title: "Amazon Clone",
         description: "A fully functional e-commerce platform built with React.js and Tailwind CSS. Features include user authentication, product browsing and shopping cart management. Provides a seamless shopping experience with real-time updates, order history tracking, and responsive design for both desktop and mobile users.",
-        imageSrc: "public/78eef5203160005.Y3JvcCwyMzEwLDE4MDcsMjUxLDE4OA.jpg",
+        imageSrc: "/78eef5203160005.Y3JvcCwyMzEwLDE4MDcsMjUxLDE4OA.jpg",
         gitLink: "https://github.com/Bansal-Karan/Amazon-Clone"
     },
     {
         title: "Bag Shop",
         description: "An online shopping platform built with Node.js, Express, EJS, and MongoDB. Includes secure user and admin authentication, product listings with detailed descriptions, and a cart feature for managing purchases. Admins can add, update, or remove products, while users can browse and buy bags seamlessly. Provides a smooth shopping experience with organized product management and responsive design.",
-        imageSrc: "public/download.jpeg",
+        imageSrc: "/download.jpeg",
         gitLink: "https://github.com/Bansal-Karan/Bag-Shop"
     },
     {
         title: "Social Media Backend",
         description: "A RESTful API backend built with Node.js, Express, and MongoDB for a social media platform. Supports user authentication, profile management, post creation, likes, comments, and follow/unfollow functionality. Includes secure JWT-based authentication, structured data modeling, and scalable endpoints to handle user interactions efficiently.",
-        imageSrc: "public/social_media_img.jpeg",
+        imageSrc: "/social_media_img.jpeg",
         gitLink: "https://github.com/Bansal-Karan/social_media_backend"
     }
 ]
